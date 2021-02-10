0b4fb76ada3752a56b82cecedb653d129d48583c
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-02-11 00:36:56 +0100

    Added comments

--- mastermind_ai.py
+++ mastermind_ai.py
@@ -6,20 +6,20 @@ from itertools import product
 # VARIABLES #
 #############
 
-combinations = list(product('abcdcd', repeat=4)) 
-combis = [''.join(i) for i in combinations[:]] 
+combinations = list(product('abcdcd', repeat=4)) # ALl possible combinations
+combis = [''.join(i) for i in combinations[:]] # Copy + cleaned up list of combinations
 code = secrets.choice(combis) 
-print('secret code is: ',code) 
+print('secret code is: ',code) # For testing
 
 
-
+# Compares feedback of guess, with possible guesses to find matching combinations
 def pins(guess, answer):
 	black = 0
 	white = 0
 	guess = list(guess)
 	answer = list(answer)
-	remainingGuess = guess[:]  
-	remainingAnswer = answer[:]  
+	remainingGuess = guess[:]  # the guessed colours that were not given a black peg
+	remainingAnswer = answer[:]  # the answer colours that were not correctly guessed
 
 	for n in range(len(guess)):
 		if answer[n] == guess[n]:
@@ -57,7 +57,7 @@ def worst_case_strategy():
 			guess = possible_guess
 	return guess
 
-
+# Can't get any simpler
 def simple_strategy():
 	return combis[0]
 
@@ -72,27 +72,27 @@ def heuristic_strategy():
 ########
 		
 def ai_play():
-	c = 0 
-	global combis 
+	c = 0 # Keeps count on amount of guesses
+	global combis # Making it global, so we can edit it
 	while True:
 		c += 1 
 		guess = heuristic_strategy() 
-		if len(combis) == 0: 
+		if len(combis) == 0: # Checks if 0 possibilites left
 			print('You probably gave wrong feedback pins. Try again')
 			quit()
 		print(f'Guess {c}: {guess}')
-		BlackPin = int(input('Black pins: ')) 
-		if BlackPin == int(len(guess)): 
+		BlackPin = int(input('Black pins: ')) # Feedback black pin
+		if BlackPin == int(len(guess)): # Success message if 4 black pins
 			print(f'Got it in {c} turns!')
 			break
-		WhitePin = int(input('White pins: ')) 
-		rightCombis = [] 
-		for possibilities in combis: 
-			if pins(guess, possibilities) == [BlackPin, WhitePin]: 
-				rightCombis.append(possibilities) 
-		combis = rightCombis[:] 
+		WhitePin = int(input('White pins: ')) # Feedback white pin
+		rightCombis = [] # for keeping the leftover possibilities
+		for possibilities in combis: # Goes through all possible combinations
+			if pins(guess, possibilities) == [BlackPin, WhitePin]: # compares feedback of all combinations with given feedback
+				rightCombis.append(possibilities) # adds combinations that have matching feedback with given feedback in one list
+		combis = rightCombis[:] # Updates combination list
 		print('Aantal mogelijkheden:',len(combis))
-		if len(combis) == 1: 
+		if len(combis) == 1: # Success message if only 1 possibility left
 			print(f'Your code is: {combis[0]}')
 			print(f'Got it in {c} turns')
 			break
@@ -100,3 +100,4 @@ def ai_play():
 			continue
 
 ai_play()
+		
